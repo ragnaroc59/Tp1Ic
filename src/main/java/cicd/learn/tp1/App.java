78772934c1d8cf3cb9d3cae797ac7d7f6f9012d8
Zeynel <zeynel59@hotmail.fr>
2019-12-04 14:54:09 +0100

    Feature 4

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "feature3 begin" );
-        System.out.println( "Feature 002" );
-        System.out.println( "CI/CD rocks!" );
+        System.out.println( "feature4 begin" );
     }
 }
